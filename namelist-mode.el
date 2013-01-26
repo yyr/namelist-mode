f1c398ee7690bafd5b08ffad8421da033528e402
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-26 11:22:50 +0900

    * namelist-mode.el: add syntax table.
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- namelist-mode.el
+++ namelist-mode.el
@@ -79,6 +79,36 @@
 
   "Key map for namelist mode.")
 
+;;; Major mode.
+(defvar namelist-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?\r " "  table) ; return is white space
+    (modify-syntax-entry ?_  "w"  table) ; underscore in names
+    (modify-syntax-entry ?\\ "\\" table) ; escape chars
+
+    (modify-syntax-entry ?! "<"  table) ; begin comment
+    (modify-syntax-entry ?\n ">"  table) ; end comment
+    (modify-syntax-entry ?\f ">"  table)
+
+    (modify-syntax-entry ?$  "_"  table) ; symbol constituents
+    (modify-syntax-entry ?\` "_"  table)
+
+    (modify-syntax-entry ?\' "\"" table) ; string quote
+    (modify-syntax-entry ?\" "\"" table)
+
+    (modify-syntax-entry ?.   "." table) ; punctuation
+    (modify-syntax-entry ?<   "." table)
+    (modify-syntax-entry ?>   "." table)
+    (modify-syntax-entry ?+  "."  table)
+    (modify-syntax-entry ?-  "."  table)
+    (modify-syntax-entry ?=  "."  table)
+    (modify-syntax-entry ?*  "."  table)
+    (modify-syntax-entry ?/  "."  table)
+    (modify-syntax-entry ?%  "."  table)
+    (modify-syntax-entry ?#  "."  table)
+    table)
+  "Syntax table used in namelist mode.")
+
 ;;;###autoload
 (define-derived-mode namelist-mode prog-mode "namelist"
   "Major mode for editing f90 namelist files.
